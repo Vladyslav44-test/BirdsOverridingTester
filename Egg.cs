9f0bea783ffe14caeda70a0862ddd3e5af1b5fc4
Vladislav44-test <elertvelert@gmail.com>
2026-09-05 18:12:19 +0300

    Додав клас Egg для позначення яйця будь-якого птаха

---
+++ Egg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdsOverridingTester
+{
+    internal class Egg
+    {
+        public double Size { get; private set; }
+        public string Color { get; private set; }
+        public string Description { get { return $"A {Size:0.0}cm {Color} egg"; } }
+
+        public Egg(double size, string color)
+        {
+            Size = size;
+            Color = color;
+        }
+    }
+}
